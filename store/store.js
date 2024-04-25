c9e53e43e6e03c9c3cd602dab820b9576eba6d63
ckswopnera <ck.swopnera@gmail.com>
2024-04-25 16:11:20 +0530

    a minor bug fix

--- store/store.js
+++ store/store.js
@@ -1,9 +1,9 @@
 import { create } from 'zustand'
 
 export const useshopApp = create((set) => ({
-  token: [],
-  removeAllBears: () => set({ token: [] }),
-  updateToken: token => set({ token }),
-  searchBar:false,
-  updateSearchBar:searchBar=>set({searchBar}),
+    token: [],
+    removeAllBears: () => set({ token: [], searchBar: false }),
+    updateToken: token => set({ token }),
+    searchBar: false,
+    updateSearchBar: searchBar => set({ searchBar }),
 }))
